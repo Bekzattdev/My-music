cd351fcb0c4b15d8d98a3e23c617046ecb65b42c
Bekzattdev <alpberlinfox@gmail.com>
2024-12-04 23:16:57 +0600

    add to  githab

---
+++ src/appPages/layout/LayoutSite.tsx
@@ -0,0 +1,17 @@
+import { FC, ReactNode } from "react";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+interface layoutProps{
+  children:ReactNode
+}
+const LayoutSite:FC<layoutProps> = ({children}) => {
+  return (
+    <div>
+<Header/>
+<main>{children}</main>
+<Footer/>
+    </div>
+  );
+};
+
+export default LayoutSite;
