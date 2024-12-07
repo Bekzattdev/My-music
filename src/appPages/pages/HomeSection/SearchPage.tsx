a0e9ea3bff49c65620080472ce2a98eaeb2eea76
Bekzattdev <alpberlinfox@gmail.com>
2024-12-07 12:10:10 +0600

    start

--- src/appPages/pages/HomeSection/SearchPage.tsx
+++ src/appPages/pages/HomeSection/SearchPage.tsx
@@ -1,31 +1,8 @@
-"use client";
-import { useGetSearchQuery } from "@/src/redux/api/search";
-import { useParams } from "next/navigation";
+import React from "react";
 
 const SearchPage = () => {
-  const params = useParams();
-  const searchQuery = Array.isArray(params.searchQuery)
-    ? params.searchQuery[0] // Если массив, взять первый элемент
-    : params.searchQuery; // Иначе просто значение
-
-  const { data, isLoading, error } = useGetSearchQuery(searchQuery || ""); // Предотвращение undefined
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error loading search results.</p>;
-console.log(data,"nonenenene");
-
   return (
     <div>
-      <h1>Search Results for &quot;{decodeURIComponent(searchQuery || "")}&quot;</h1>
-      <ul>
-        {data?.results?.map(
-          (song: { id: string; title: string; artist: string }) => (
-            <li key={song.id}>
-              {song.title} by {song.artist}
-            </li>
-          )
-        )}
-      </ul>
     </div>
   );
 };
--- src/redux/api/search/index.ts
+++ src/redux/api/search/index.ts
@@ -1,14 +1,14 @@
-import { api as index } from "..";
+// import { api as index } from "..";
 
-const api = index.injectEndpoints({
-  endpoints: (build) => ({
-    getSearch: build.query({
-      query: (query: string) => ({
-        url: `/search?q=${query}`,
-        method: "GET",
-      }),
-      providesTags: ["search"],
-    }),
-  }),
-});
-export const { useGetSearchQuery } = api;
+// const api = index.injectEndpoints({
+//   endpoints: (build) => ({
+//     getSearch: build.query({
+//       query: (query: string) => ({
+//         url: `/search?q=${query}`,
+//         method: "GET",
+//       }),
+//       providesTags: ["search"],
+//     }),
+//   }),
+// });
+// export const { useGetSearchQuery } = api;
